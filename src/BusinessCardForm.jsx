71e4457bd7623029ea8f61422b5d1fef99bf9f99
Burim Saliji <buz.saliji@gmail.com>
2023-09-21 20:36:10 +1000

    feat: htmlFor instead of for

--- src/BusinessCardForm.jsx
+++ src/BusinessCardForm.jsx
@@ -1,19 +1,22 @@
+import { Component } from "react";
 
 
 export default class BusinessCardForm extends Component {
-    constructor(){
-        super();
-        this.state = {
-            name: this.props.name,
-            email: this.props.email
-        }
-    }
-    render(){
-        return(
-            <form>
-                <label for="nameInput">Name:</label>
-                <input type="text" name="nameInput" id="nameInput" />
-            </form>
-        )
-    }
+	constructor(props){
+		super(props);
+
+		this.state = {
+			name: this.props.name,
+			email: this.props.email
+		}
+	}
+
+	render(){
+		return(
+			<form>
+				<label htmlFor="nameInput" >Name:</label>
+				<input type="text" name="nameInput" id="nameInput" />
+			</form>
+		)
+	}
 }
