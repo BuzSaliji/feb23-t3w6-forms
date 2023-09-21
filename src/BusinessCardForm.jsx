493e0026a17ecd92f12cd7e119ebeaf4ced7a089
Burim Saliji <buz.saliji@gmail.com>
2023-09-21 21:01:03 +1000

    feat: removing state from form

--- src/BusinessCardForm.jsx
+++ src/BusinessCardForm.jsx
@@ -2,21 +2,23 @@ import { Component } from "react";
 
 
 export default class BusinessCardForm extends Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-			name: this.props.name,
-			email: this.props.email
-		}
-	}
+	     handleChangeInput = (event) => {
+        // pass in the form input data
+        // call the parent setState function
+        this.props.setParentState(event.target.name, event.target.value);
+    }
 
 	render(){
 		return(
 			<form>
                 {/* htmlFor instead of for for the JSx compiler */}
 				<label htmlFor="nameInput" >Name:</label>
-				<input type="text" name="nameInput" id="nameInput" value={this.state.name} />
+				<input type="text" 
+                name="nameInput" 
+                id="nameInput" 
+                value={this.props.name} 
+                onChange={(event) => this.props.setParentState("name", event.target.value)}
+                />
 			</form>
 		)
 	}
