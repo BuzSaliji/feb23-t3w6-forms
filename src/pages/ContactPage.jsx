dc1f6736f9bd86b1bc6e83b5c6392dd247252732
Burim Saliji <buz.saliji@gmail.com>
2023-09-23 12:01:50 +1000

    feat: Redirect home button

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,12 @@
+import RedirectHomeButton from "../components/RedirectHomeButton";
+
+
 export default function ContactPage(props){
 	return(
 		<div>
 			<h1>Call me</h1>
+
+			<RedirectHomeButton />
 		</div>
 	)
 }
